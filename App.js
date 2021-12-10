3e031e6f5e0672dec3439e9459c66d57e485f15c
OSabio <gahsabio@outlook.com>
2021-12-10 14:58:14 -0300

    projeto

--- App.js
+++ App.js
@@ -1,21 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { Text } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { SafeArea } from "./src/utility/safe-area.components";
+import { Ionicons } from "@expo/vector-icons";
+import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
+import { LocationContextProvider } from "./src/services/location/location.context";
+
+const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Padarias: "md-restaurant",
+  Mapa: "md-map",
+  Ajustes: "md-settings",
+};
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Ajustes</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Mapa</Text>
+  </SafeArea>
+);
+
+const createSreenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+  };
+};
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ThemeProvider theme={theme}>
+        <LocationContextProvider>
+          <RestaurantsContextProvider>
+            <NavigationContainer>
+              <Tab.Navigator screenOptions={createSreenOptions}>
+                <Tab.Screen name="Padarias" component={RestaurantsScreen} />
+                <Tab.Screen name="Mapa" component={Map} />
+                <Tab.Screen name="Ajustes" component={Settings} />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </RestaurantsContextProvider>
+        </LocationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/services/restaurants/mock/index.js
@@ -0,0 +1,21 @@
+import antwerp from "./antwerp.json";
+import chicago from "./chicago.json";
+import toronto from "./toronto.json";
+import san_francisco from "./san_francisco.json";
+
+export const mocks = {
+  "51.219448,4.402464": antwerp,
+  "43.653225,-79.383186": toronto,
+  "41.878113,-87.629799": chicago,
+  "37.7749295,-122.4194155": san_francisco
+};
+
+export const mockImages = [
+  "https://media.istockphoto.com/photos/coffee-shop-picture-id1279333349?b=1&k=20&m=1279333349&s=170667a&w=0&h=eydkidMjYaGkFS_kt-6TTzKxH6PJs4ZWMUq6RnhQRFg=",
+  "https://media.istockphoto.com/photos/bread-and-buns-picture-id496564915?b=1&k=20&m=496564915&s=170667a&w=0&h=PcEHQJsT2jvnyOeJqnLn0IWK502dhzJc32qOiW6ut4Q=",
+  "https://media.istockphoto.com/photos/bakery-shop-interior-with-various-breads-and-buns-on-the-shelves-picture-id1311800432?b=1&k=20&m=1311800432&s=170667a&w=0&h=DrEW3X9xoYF5WfOa5tE-YDnUS3R4FrO730k19r7y44k=",
+  "https://media.istockphoto.com/photos/image-of-a-cafeteria-in-large-office-picture-id1298318527?b=1&k=20&m=1298318527&s=170667a&w=0&h=1-gH4Xa4wNKFoOUDP2mu3ne2icOQW8z0nSPr8-ylDHk=",
+  "https://media.istockphoto.com/photos/fresh-cakes-picture-id497959594?b=1&k=20&m=497959594&s=170667a&w=0&h=bARJnh0s5Udce7K30WPdeVnud23lVg5WngzD9Jral9w=",
+  "https://media.istockphoto.com/photos/hands-baking-dough-with-rolling-pin-on-wooden-table-picture-id673400318?b=1&k=20&m=673400318&s=170667a&w=0&h=seiblIlg_Xp7tDg48tm0VODK2y1_yPRzYJ7TUTp2GcQ=",
+  "https://www.foodiesfeed.com/wp-content/uploads/2019/02/pizza-ready-for-baking-600x400.jpg"
+];
